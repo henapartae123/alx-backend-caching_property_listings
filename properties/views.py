5a8781ab2a200fd7bacab6106fd82c77d122a739
henapartae123 <liyulabs@gmail.com>
2025-11-05 22:03:22 +0300

    views

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .utils import get_all_properties
+
+
+@cache_page(60 * 15)  # Cache view response for 15 minutes
+def property_list(request):
+    """
+    View to return all properties.
+    Uses low-level cache API to cache queryset for 1 hour.
+    View response is also cached for 15 minutes.
+    """
+    response = get_all_properties()
+
+    # Extract properties list from the response
+    properties = response.get('data', [])
+
+    return JsonResponse({
+        'count': len(properties),
+        'data': properties
+    }, safe=False)
